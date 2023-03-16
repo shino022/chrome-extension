622b9879a0cde87254e242599ae62d22d41df068
shin <sshoony95@gmail.com>
2023-03-15 20:11:40 -0400

    Return error to client

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,8 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.post('/', function(req, res, next) {
-  req.body
-  console.log(req.body);
+  console.log(req.body.text.split(' ').length);
   fetch("https://api.openai.com/v1/chat/completions", {
     method: "POST",
     headers: {
@@ -17,13 +16,11 @@ router.post('/', function(req, res, next) {
 })
     .then((response) => response.json())
     .then((data) => {
-      console.log(data);
+      if(data.error) {
+        return res.status(400).json(data.error)
+      }
       res.json({summary: data.choices[0].message.content})
     })
-    .catch((err)=>{
-      console.log(err);
-      res.status(401).json(err);
-    });
 });
 
 module.exports = router;
