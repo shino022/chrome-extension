a4ba31f0d37b906d0f47dfdbf65d20c6196273f6
shin <sshoony95@gmail.com>
2023-03-19 18:25:54 -0400

    Handle fetch's error

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,31 +1,37 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-router.post('/', function(req, res, next) {
-  const tokens = req.body.text.split(' ');
-  if(tokens.length > 2020) {
-    return res.status(400).json({error: {
-      message: "The selected text exceeds the maximum limit of 2000 words. Please select a shorter text and try again."
-    }})
+router.post("/", function (req, res, next) {
+  const tokens = req.body.text.split(" ");
+  if (tokens.length > 2020) {
+    return res.status(400).json({
+      error: {
+        message:
+          "The selected text exceeds the maximum limit of 2000 words. Please select a shorter text and try again.",
+      },
+    });
   }
   fetch("https://api.openai.com/v1/chat/completions", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
-      "Authorization": `Bearer ${process.env.TOKEN}`
+      Authorization: `Bearer ${process.env.TOKEN}`,
     },
     body: JSON.stringify({
-      "model": "gpt-3.5-turbo",
-      "messages": [{"role": "user", "content": `${req.body.text}`}]
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "user", content: `${req.body.text}` }],
     }),
-})
+  })
     .then((response) => response.json())
     .then((data) => {
-      if(data.error) {
-        return res.status(400).json(data)
+      if (data.error) {
+        return res.status(400).json(data);
       }
-      res.json({summary: data.choices[0].message.content})
+      res.json({ summary: data.choices[0].message.content });
     })
+    .catch((err) => {
+      return res.status(500).json(err);
+    });
 });
 
 module.exports = router;
