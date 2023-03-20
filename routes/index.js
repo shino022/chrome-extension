c06276363372f5848c75c271548101b0ac55fca2
shin <sshoony95@gmail.com>
2023-03-20 13:49:16 -0400

    Use axios

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var router = express.Router();
+var axios = require ('axios');
 
 router.get("/", (req, res) => {
   res.send("<h1>Server</h1>")
@@ -15,25 +16,29 @@ router.post("/", function (req, res, next) {
       },
     });
   }
-  fetch("https://api.openai.com/v1/chat/completions", {
+  axios({
+    url: "https://api.openai.com/v1/chat/completions",
     method: "POST",
     headers: {
       "Content-Type": "application/json",
       Authorization: `Bearer ${process.env.TOKEN}`,
     },
-    body: JSON.stringify({
+    data: JSON.stringify({
       model: "gpt-3.5-turbo",
       messages: [{ role: "user", content: `${req.body.text}` }],
     }),
   })
-    .then((response) => response.json())
+    .then((response) => response.data)
     .then((data) => {
+      console.log(data);
+
       if (data.error) {
         return res.status(400).json(data);
       }
       res.json({ summary: data.choices[0].message.content });
     })
     .catch((err) => {
+      console.log(err);
       return res.status(500).json(err);
     });
 });
